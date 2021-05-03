88f05ad0be2b05c35945f35dab63cde4c929da51
RubenEu <garcia.ruben@outlook.es>
2021-05-03 09:01:04 +0200

    Cambios en las utils

--- vehicle_speed_estimation/utils/__init__.py
+++ vehicle_speed_estimation/utils/__init__.py
@@ -1,4 +1,5 @@
 from vehicle_speed_estimation.utils.homography import (apply_homography_object,
+                                                       apply_homography_objects,
                                                        apply_homography_tracked_objects,
                                                        apply_homography_point2d,
                                                        apply_homography_frame)
