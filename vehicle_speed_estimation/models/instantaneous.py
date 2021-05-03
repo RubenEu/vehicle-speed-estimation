f804996ee59cf0b761073a8d0fa3c0843edfcdb4
RubenEu <garcia.ruben@outlook.es>
2021-05-04 01:18:48 +0200

    VSE-14: modificado el kernel gaussiano

--- vehicle_speed_estimation/models/instantaneous.py
+++ vehicle_speed_estimation/models/instantaneous.py
@@ -123,11 +123,13 @@ class InstantaneousVelocityWithKernelRegression(InstantaneousVelocity):
 
     @staticmethod
     def kernel_gauss(t, t_i, h):
-        return np.exp(- (t - t_i) ** 2 / h)
+        # return np.exp(- (t - t_i) ** 2 / h)
+        return np.exp(- (t - t_i) ** 2 / ((2 * h) ** 2))
 
     @staticmethod
     def kernel_gauss_derivated(t, t_i, h):
-        return - 2 / h * (t - t_i) * np.exp(- (t - t_i) ** 2 / h)
+        # return - 2 / h * (t - t_i) * np.exp(- (t - t_i) ** 2 / h)
+        return - ((2 * t - 2 * t_i) / (2 * h ** 2)) * np.exp(- ((t - t_i) ** 2) / (2 * h ** 2))
 
     @staticmethod
     def kernel_triangular(t, t_i, h):
