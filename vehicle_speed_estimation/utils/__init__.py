2bca2d7bf5661d068a7e0460c5422a77c96ccab6
RubenEu <garcia.ruben@outlook.es>
2021-05-17 17:18:01 +0200

    Movidas las utils de geometría de VSE a SOT

--- vehicle_speed_estimation/utils/__init__.py
+++ vehicle_speed_estimation/utils/__init__.py
@@ -4,5 +4,3 @@ from vehicle_speed_estimation.utils.homography import (apply_homography_object,
                                                        apply_homography_tracked_objects,
                                                        apply_homography_point2d,
                                                        apply_homography_frame)
-from vehicle_speed_estimation.utils.geometry import (point_distance_to_line,
-                                                     find_closest_position_to_line)
